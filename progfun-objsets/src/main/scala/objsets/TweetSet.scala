b0183c7cb5a0a434a46f96352f0f1e73044dabd6
Peixiang xu <peixiangxu@gmail.com>
2013-10-03 13:44:04 +0800

    fix performance problem

--- progfun-objsets/src/main/scala/objsets/TweetSet.scala
+++ progfun-objsets/src/main/scala/objsets/TweetSet.scala
@@ -137,10 +137,9 @@ class Empty extends TweetSet {
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = 
-    if(p(elem)) left.filterAcc(p,acc incl elem) union right.filterAcc(p,acc incl elem) 
-    else left.filterAcc(p,acc).union(right.filterAcc(p,acc)) 
-  
-  def union(that: TweetSet): TweetSet = ((left union right) union that) incl elem
+    if(p(elem)) right.filterAcc(p, left.filterAcc(p, acc).incl(elem)) else right.filterAcc(p, left.filterAcc(p, acc))
+
+  def union(that: TweetSet): TweetSet = (left union (right union that)) incl elem
   
   def mostRetweeted: Tweet = 
     if ((left.isEmpty && right.isEmpty )|| this.moreRetweeted.isEmpty ) elem
