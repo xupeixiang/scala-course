501e9acd0a6c4d3a6684838a1575cf0a11985bc2
Peixiang xu <peixiangxu@gmail.com>
2013-10-11 21:12:50 +0800

    add apply demo

--- OnClass/src/week3/List.scala
+++ OnClass/src/week3/List.scala
@@ -13,6 +13,12 @@ class Cons[T](val head: T, var tail: List[T]) extends List[T]{
 class Nil[T] extends List[T]{
   def isEmpty = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
-  def tail: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
+object List {
+  // List(1,2) = List.apply(1, 2)
+  def apply[T](x1: T,x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+  def apply[T](x: T) = new Cons(x, new Nil)
+}
