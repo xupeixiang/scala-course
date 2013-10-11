3ae1837fc0ecdae8def960f364ab795f5d97c8ef
Peixiang xu <peixiangxu@gmail.com>
2013-10-11 22:50:38 +0800

    add variance demo

--- OnClass/src/week3/List.scala
+++ OnClass/src/week3/List.scala
@@ -1,16 +1,17 @@
 package week3
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+  def prepend [U >: T] (elem: U): List[U] = new Cons(elem, this)
 }
 
 class Cons[T](val head: T, var tail: List[T]) extends List[T]{
   def isEmpty = false
 }
 
-class Nil[T] extends List[T]{
+object Nil extends List[Nothing]{
   def isEmpty = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
@@ -21,4 +22,8 @@ object List {
   def apply[T](x1: T,x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
   def apply[T]() = new Nil
   def apply[T](x: T) = new Cons(x, new Nil)
+}
+
+object test{
+  val x: List[String] = Nil
 }
