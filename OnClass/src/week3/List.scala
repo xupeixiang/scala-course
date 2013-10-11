d7f2c56f5b72b4aaad6a0b5a7ef973b96645d5eb
Peixiang xu <peixiangxu@gmail.com>
2013-10-12 07:59:25 +0800

    fix bug of object Nil

--- OnClass/src/week3/List.scala
+++ OnClass/src/week3/List.scala
@@ -19,9 +19,9 @@ object Nil extends List[Nothing]{
 
 object List {
   // List(1,2) = List.apply(1, 2)
-  def apply[T](x1: T,x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
-  def apply[T]() = new Nil
-  def apply[T](x: T) = new Cons(x, new Nil)
+  def apply[T](x1: T,x2: T): List[T] = new Cons(x1, new Cons(x2, Nil))
+  def apply[T]() = Nil
+  def apply[T](x: T) = new Cons(x, Nil)
 }
 
 object test{
