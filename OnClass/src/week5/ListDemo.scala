114e35db861e56cf17f8f21b89b5086c3223d34a
Peixiang xu <peixiangxu@gmail.com>
2013-10-19 15:11:23 +0800

    add concat exercise

--- OnClass/src/week5/ListDemo.scala
+++ OnClass/src/week5/ListDemo.scala
@@ -1,9 +1,14 @@
 package week5
 
 object ListDemo {
-	def init[T](xs: List[T]) : List[T] = xs match {
+	def init[T](xs: List[T]): List[T] = xs match {
 	  case List() => throw new Error("init of empty list")
 	  case List(x) => List(x)
-	  case y :: ys => List(y) ::: this.drop(1).init[T](ys)
+	  case y :: ys => y :: init(ys)
+	}
+	
+	def concat[T](xs: List[T], ys: List[T]): List[T] = xs match {
+	  case List() => ys
+	  case z :: zs => z :: concat(zs, ys)
 	}
 }
