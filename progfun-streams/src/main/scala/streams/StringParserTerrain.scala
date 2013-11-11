696eebc9034645f72fdd1496eef5c49fbbd1e0b9
Peixiang xu <peixiangxu@gmail.com>
2013-11-11 23:16:18 +0800

    finish terrain functions

--- progfun-streams/src/main/scala/streams/StringParserTerrain.scala
+++ progfun-streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,8 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = 
+    p => 0 <= p.x && p.x < levelVector.length && 0 <= p.y && p.y < levelVector.apply(p.x).length
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +63,9 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = 
+    (for (row <- (0 until levelVector.length).toStream; if(levelVector.apply(row).indexOf(c) >= 0)) 
+      yield new Pos(row, levelVector.apply(row).indexOf(c))).apply(0)
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
